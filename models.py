7a9ec9b906dd314ff36381542daa6c4ca21a225d
19Pablo19 <p.alcazar04@gmail.com>
2018-12-16 21:53:50 +0100

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-#Clase para las peliculas
-class Filmsq():
-    def __init__(self,name, quantity):
-        self.name = name
-        self.quantity = quantity
-
-    def p(self):
-        cadena = {'name':self.name, 'quantity':self.quantity}
-        return cadena
